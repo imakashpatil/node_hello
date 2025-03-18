24599bb48bdf766114b2ec078fdec4da8f87e27d
Akash Patil <akashpatil@Mac.lan>
2025-03-18 18:07:24 +0530

    file renamed

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello, World! Runing on PORT 3000');
 });
-
+app.get('/home', (req, res) => {
+    res.send('This is home page');
+});
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
